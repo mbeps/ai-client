253df06fbd723c81a5b5fc8475f23d4df7ac48f2
Maruf Bepary <bepary71@gmail.com>
2026-05-13 15:35:18 +0100

    refactor: remove unused slug utilities, mock data, and testing scripts

--- test-db.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "./drizzle/db";
-import { project } from "./drizzle/schema";
-
-async function test() {
-  const projects = await db.select().from(project);
-  console.log(projects);
-}
-
-test().catch(console.error);
